8764f0ea6a7ea69164b49b0560ca69e04c9299ff
Alex <webmaster@papagai.de>
2022-02-10 14:55:58 +0100

    fix

--- script.js
+++ script.js
@@ -1,53 +1,72 @@
 "use strict";
 
 
-const dataTime=function(){
-  setInterval(() => {
-    let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes()
-    let seconds = date.getSeconds()
+
+const dataTime = function () {
+
+  let date = new Date()
+  let hour = date.getHours()
+  let minute = date.getMinutes()
+  let seconds = date.getSeconds()
   
 
-    let dateNumber = new Date().toLocaleString('ru', {
-      day: 'numeric',
-      month: 'numeric',
-      year: 'numeric',
 
-    });
+  let dateNumber = new Date().toLocaleString('ru', {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
 
-    let dateBase = new Date().toLocaleString('ru', {
-      weekday: 'long',
-      year: 'numeric',
-      day: 'numeric',
-      month: 'long',
+  });
 
-    })
-      .replace('г.', 'года')
+  let dateBase = new Date().toLocaleString('ru', {
+    weekday: 'long',
+    year: 'numeric',
+    day: 'numeric',
+    month: 'long',
 
-    
-    const hourEnd = function () {
+  })
+    .replace('г.', 'года')
 
-      if (hour % 10 == 1) return 'час'
-      if (hour % 10 == 2 || hour % 10 == 3 || hour % 10 == 4) {
-        return 'часa'
-      } else return 'часов'
 
-    }
-    const check = function (time) {
-      if (time.toString().length < 2) {
-        return time = '0' + time
-      } else return time
+  const hourEnd = function () {
 
-    } 
+    if (hour == 12 || hour == 11) return 'часов'
+    if (hour % 10 == 1) return 'час'
+    if (hour % 10 == 2 || hour % 10 == 3 || hour % 10 == 4) {
+      return 'часa'
+    } else return 'часов'
 
-    document.getElementsByTagName('b')[0].innerHTML = `b)  ${dateNumber} - ${check(hour)} : ${check(minute)} : ${check(seconds)} `;
-    
-    
-    document.getElementsByTagName('h4')[0].innerHTML = `а) Сегодня ${dateBase[0].toUpperCase() + dateBase.slice(1)} ${hour} ${hourEnd()} ${minute} минут ${seconds} секунды `;
-    
+  }
 
-  }, 1000)
+  const timeEnd = function (time) {
+
+    if (time !== 11 && time % 10 == 1) return 'a'
+    if ((time < 10 || time > 20) && time % 10 == 2 || time % 10 == 3 || time % 10 == 4) {
+      return 'ы'
+    } else return ''
+
+  }
+
+  const check = function (time) {
+    if (time.toString().length < 2) {
+      return time = '0' + time
+    } else return time
+
+  }
+  
+  let minuteNum = check(minute)
+  let hourNum = check(hour)
+  let secondsNum = check(seconds)
+  let weekDayVariable =  dateBase[0].toUpperCase() + dateBase.slice(1)
+  
+
+  document.getElementsByTagName('b')[0].innerHTML = `b)  ${dateNumber} - ${hourNum} : ${minuteNum} : ${secondsNum} `;
+
+
+  document.getElementsByTagName('h4')[0].innerHTML = `а) Сегодня ${weekDayVariable} ${hour} ${hourEnd()} ${minute} минут${timeEnd(minute)} ${seconds} секунд${timeEnd(seconds)} `;
+
+
+
 
 }
 
@@ -57,6 +76,8 @@ document.body.appendChild(div);
 const div2 = document.createElement('b');
 document.body.appendChild(div2);
 dataTime()
+setInterval(dataTime, 1000)
+
 
 
 
